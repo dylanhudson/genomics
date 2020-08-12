eee847a0cfd6ae49dcf9b6117370de75ea232cf9
Dylan Hudson <dylan@dylans-MacBook-Pro.local>
2020-08-11 21:21:58 -0600

    initial upload of contig filter

---
+++ contig_length_filter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 20:58:28 2020
+
+@author: dylan
+"""
+
+#import os
+#import sys
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser(description='filename, then contig length threshold')
+
+parser.add_argument('filename', type=str, help='path to fasta file')
+parser.add_argument('min_length', type=int, help='minimum length of contigs to allow.')
+
+args = parser.parse_args()
+
+input_file = args.filename
+
+min_contig_length = args.min_length
+
+
+my_seq = SeqIO.parse(input_file, "fasta")
+
+#placeholder list construct
+longer_contigs = []
+filter_counter = 0
+
+for record in my_seq:
+    if len(record.seq) > min_contig_length:
+        longer_contigs.append(record)
+    else:
+        filter_counter = filter_counter + 1
+
+
+SeqIO.write(longer_contigs, ("filtered_" + input_file), "fasta" )
+print("Contigs removed = ")
+print(filter_counter)
+        
+
